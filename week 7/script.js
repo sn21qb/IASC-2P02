72d98295f19de19fd51fff273eb6943f769f1e10
sn21qb <sn21qb@brocku.ca>
2024-02-29 19:44:42 -0500

    interactive essay draft 1

---
+++ week 7/script.js
@@ -0,0 +1,194 @@
+import * as THREE from "three"
+import * as dat from "lil-gui"
+import { OrbitControls } from "OrbitControls"
+
+/**********
+** SETUP **
+***********/
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    aspectRatio: window.innerWidth / window.innerHeight
+}
+
+/**********
+** SCENE **
+***********/
+// Canvas
+const canvas = document.querySelector('.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.aspectRatio,
+    0.1,
+    100
+)
+camera.position.set(1, 0, 0)
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/***********
+** MESHES **
+************/
+const caveMaterial = new THREE.MeshStandardMaterial({
+    color: new THREE.Color('white'),
+    side: THREE.DoubleSide
+})
+
+// caveWall
+const caveWallGeometry = new THREE.PlaneGeometry(10, 5)
+const caveWall = new THREE.Mesh(caveWallGeometry, caveMaterial)
+caveWall.rotation.y = Math.PI * 0.5
+caveWall.position.set(-5, 0, 0)
+caveWall.receiveShadow = true
+scene.add(caveWall)
+
+// barrierWall
+const barrierWallGeometry = new THREE.PlaneGeometry(10, 2)
+const barrierWall = new THREE.Mesh(barrierWallGeometry, caveMaterial)
+barrierWall.rotation.y = Math.PI * 0.5
+barrierWall.position.set(5, -1.5, 0)
+scene.add(barrierWall)
+
+// caveFloor
+const caveFloorGeometry = new THREE.PlaneGeometry(10, 10)
+const caveFloor = new THREE.Mesh(caveFloorGeometry, caveMaterial)
+caveFloor.rotation.x = Math.PI * 0.5
+caveFloor.position.set(0, -2.5, 0)
+scene.add(caveFloor)
+
+// OBJECTS
+// torusKnot
+const IcosahedronGeometry = new THREE.IcosahedronGeometry(1,0);
+const IcosahedronMaterial = new THREE.MeshNormalMaterial()
+const Icosahedron = new THREE.Mesh(IcosahedronGeometry, IcosahedronMaterial)
+Icosahedron.position.set(6, 1.5, 0)
+Icosahedron.castShadow = true
+scene.add(Icosahedron)
+
+// SUN
+const sunGeometry = new THREE.SphereGeometry()
+const sunMaterial = new THREE.MeshLambertMaterial({
+    emissive: new THREE.Color('orange'),
+    emissiveIntensity: 20
+})
+const sun = new THREE.Mesh(sunGeometry, sunMaterial)
+scene.add(sun)
+
+/***********
+** LIGHTS **
+************/
+/*
+// Ambient Light
+const ambientLight = new THREE.AmbientLight(
+    new THREE.Color('white')
+)
+scene.add(ambientLight)
+*/
+
+// Direcional Light
+const directionalLight = new THREE.DirectionalLight(
+    new THREE.Color('white'),
+    0.5
+)
+directionalLight.target = caveWall
+directionalLight.position.set(10, 2.5, 0)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.width = 1024
+directionalLight.shadow.mapSize.height = 1024
+scene.add(directionalLight)
+
+// Directional Light Helper
+//const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight)
+//scene.add(directionalLightHelper)
+
+/*******
+** UI **
+********/
+const ui = new dat.GUI()
+
+const uiObject = {}
+
+uiObject.reset = () =>
+{
+    directionalLight.position.set(8.6, 1.7, 0)
+}
+
+// Directional Light
+const lightPositionFolder = ui.addFolder('Directional Light Position')
+
+lightPositionFolder
+    .add(directionalLight.position, 'x')
+    .min(-10)
+    .max(20)
+    .step(0.1)
+
+lightPositionFolder
+    .add(directionalLight.position, 'y')
+    .min(-10)
+    .max(10)
+    .step(0.1)
+
+lightPositionFolder
+    .add(directionalLight.position, 'z')
+    .min(-10)
+    .max(10)
+    .step(0.1)
+
+lightPositionFolder
+    .add(uiObject, 'reset')
+    .name('Reset position')
+
+
+/*******************
+** ANIMATION LOOP **
+********************/
+const clock = new THREE.Clock()
+
+// Animate
+const animation = () =>
+{
+    // Return elapsedTime
+    const elapsedTime = clock.getElapsedTime()
+
+    // Animate Objects
+    Icosahedron.rotation.y = elapsedTime
+    Icosahedron.position.z = Math.sin(elapsedTime * 0.5) * 2
+
+    // Update directionalLightHelper
+    //directionalLightHelper.update()
+
+    // Update sun position to match directionalLight position
+    sun.position.copy(directionalLight.position)
+
+    console.log(camera.position)
+
+    // Controls
+    controls.update()
+
+    // Renderer
+    renderer.render(scene, camera)
+
+    // Request next frame
+    window.requestAnimationFrame(animation)
+}
+
+animation()
+
